4477e52210c2f2de13faff46f99ef8400097b335
anikeith22 <anikeith.bankar@mail.utoronto.ca>
2021-10-23 02:59:52 -0400

    created base searching html css files

---
+++ app/searching_filtering/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, SelectField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired, EqualTo, Length, ValidationError
+
