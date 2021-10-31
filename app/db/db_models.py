76df2385f150af088eccdc69bb23931433a525c8
anikeith22 <anikeith.bankar@mail.utoronto.ca>
2021-10-31 17:31:18 -0400

    added models for all the database tables for all the features

--- app/db/db_models.py
+++ app/db/db_models.py
@@ -1,11 +1,54 @@
+from os import name
 from app import dbsql
+from sqlalchemy import Table, Column, Integer, ForeignKey, Boolean, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin # Helps in managing a user session
 
-class ExampleTable(dbsql.Model):
-    id = dbsql.Column(dbsql.Integer, primary_key=True)
-
-class User(dbsql.Model, UserMixin):
+Base = declarative_base()
+class User(Base, UserMixin):
+    __tablename__ = 'user'
     id = dbsql.Column(dbsql.Integer, primary_key = True) # Generated automatically when not specified
     username = dbsql.Column(dbsql.String(100), unique = True, nullable = False)
     email = dbsql.Column(dbsql.String(100), unique = True, nullable = False)
     password = dbsql.Column(dbsql.String(100), nullable = False)
+
+    def __repr__(self):
+        return f"User(UserId: '{self.id}', Username: '{self.username}', email: '{self.email}')"
+class Courses(Base):
+    __tablename__ = 'courses'
+    courseId = dbsql.Column(dbsql.String(8), primary_key = True)
+    name = dbsql.Column(dbsql.String, nullable = False)
+    description = dbsql.Column(dbsql.String, nullable = False)
+    department = dbsql.Column(dbsql.String, nullable = False)
+    prerequisites = dbsql.Column(dbsql.String, nullable = False)
+    course_level = dbsql.Column(dbsql.Integer, nullabe=False)
+    campus = dbsql.Column(dbsql.String, nullable=False)
+    terms_offered = dbsql.Column(dbsql.String,  nullable=False)
+    exclusion = dbsql.Column(dbsql.String, nullable=False)
+    corequisites = dbsql.Column(dbsql.String, nullable=False)
+    fase_availabe = dbsql.Column(dbsql.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f"Course(Course Id: '{self.courseId}', Course Name: '{self.name}')"
+
+class UserSavedCourses(Base):
+    __tablename__ = 'user_saved_courses'
+    userId = dbsql.Column(dbsql.Integer, ForeignKey('user.id'), primary_key = True) 
+    courseId = dbsql.Column(dbsql.String(8), ForeignKey('courses.id'), primary_key = True)
+    users = relationship("User")
+    courses = relationship("Courses")
+    def __repr__(self):
+        return f"UserSavedCourses('User: {self.userId}', Saved Course: '{self.courseId}')"
+
+class CourseComments(Base):
+    __tablename__ = 'course_comments'
+    userId = dbsql.Column(dbsql.Integer, ForeignKey('user.id'), primary_key = True)
+    courseId = dbsql.Column(dbsql.String(15), ForeignKey('courses.id'), primary_key = True)
+    comment = courseId = dbsql.Column(dbsql.String, ForeignKey('courses.id'), primary_key = True)
+    users = relationship("User")
+    courses = relationship("Courses")
+
+    def __repr__(self):
+        return f"CourseComments(User: '{self.userId}', Course: '{self.courseId}', Comment: '{self.comment}')"
+
