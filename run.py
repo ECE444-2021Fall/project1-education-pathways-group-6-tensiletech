2b373e5b9816e15cc644be1b731512b9b5f4b024
Yi Jia Zhang <yijia.zhang11@gmail.com>
2021-11-16 13:44:12 -0500

    remove the main scope in run.py for heroku to detect the app variable

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
 import os
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(os.getenv('FLASK_CONFIG') or "default")
+app = create_app()
+app.run(os.getenv('FLASK_CONFIG') or "default")
 
